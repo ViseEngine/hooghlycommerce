00665c4dc3284dfab9207bc415bcf2041b7696ca
dhrubo <dhrubo.kayal@gmail.com>
2017-12-30 08:42:12 -0600

    minor fixes

--- orderflo/src/main/java/co/hooghly/commerce/orderflo/MvcConfiguration.java
+++ orderflo/src/main/java/co/hooghly/commerce/orderflo/MvcConfiguration.java
@@ -1,32 +1,14 @@
 package co.hooghly.commerce.orderflo;
 
-import java.util.List;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-
-import co.hooghly.commerce.orderflo.controller.interceptor.UiMetaDataPopulator;
-
 
 @Configuration
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
 	
 	
-	@Bean
-	public UiMetaDataPopulator uiMetaDataLoadingInterceptor(){
-		return BeanUtils.instantiate(UiMetaDataPopulator.class);
-	}
-	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(uiMetaDataLoadingInterceptor()).excludePathPatterns("/webjars/**");
-	}
+
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
@@ -34,8 +16,5 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 		
 	}
 
-	@Override
-	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-	//	argumentResolvers.add(new DatatablesRequestMethodArgumentResolver());
-	}
+	
 }
--- orderflo/src/main/java/co/hooghly/commerce/orderflo/OrderfloApplication.java
+++ orderflo/src/main/java/co/hooghly/commerce/orderflo/OrderfloApplication.java
@@ -1,11 +1,8 @@
 package co.hooghly.commerce.orderflo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-//@EnableAutoConfiguration
 @SpringBootApplication
 public class OrderfloApplication {
 
